9b895e0acc4dfa1a6d8ae3a3b204b992cb6e34d2
Conor Shields <42182485+cshields236@users.noreply.github.com>
2019-11-28 18:23:08 +0000

    users can register and log in using email

--- app/src/main/java/com/example/assignment3/LogInActivity.java
+++ app/src/main/java/com/example/assignment3/LogInActivity.java
@@ -1,14 +1,115 @@
 package com.example.assignment3;
 
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+
 
 public class LogInActivity extends AppCompatActivity {
+
+    FirebaseAuth mFirebaseAuth;
+    public FirebaseAuth.AuthStateListener mAuthStateListener;
+    private FirebaseAuth mAuth;
+    EditText emailtxt;
+    EditText passwordtxt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+        Intent i = getIntent();
+
+        if (i == null) {
+            String e = i.getStringExtra("email");
+            emailtxt.setText(e);
+
+            TextView register = findViewById(R.id.lLink);
+
+
+            register.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent i = new Intent(LogInActivity.this, RegisterActivity.class);
+                    startActivity(i);
+                }
+            });
+        } else {
+
+
+            TextView register = findViewById(R.id.lLink);
+
+
+            register.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent i = new Intent(LogInActivity.this, RegisterActivity.class);
+                    startActivity(i);
+                }
+            });
+        }
+
     }
+
+    public void signIn(View view) {
+        mAuth = FirebaseAuth.getInstance();
+
+
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        emailtxt = findViewById(R.id.lEmail);
+        passwordtxt = findViewById(R.id.lPassword);
+
+        String email = emailtxt.getText().toString();
+        String password = passwordtxt.getText().toString();
+        // waiting = new SpotsDialog.Builder().setContext(this).setMessage("Logging In").setCancelable(false).build();
+        if (email.isEmpty()) {
+
+            emailtxt.setError("Please enter an email");
+            emailtxt.requestFocus();
+        } else if (password.isEmpty()) {
+            passwordtxt.setError("Please enter password");
+            passwordtxt.requestFocus();
+        } else if (email.isEmpty() && password.isEmpty()) {
+            Toast.makeText(LogInActivity.this, "Please Enter an Email and Password to Sign In", Toast.LENGTH_LONG).show();
+        } else if (!((email.isEmpty() && password.isEmpty()))) {
+
+            mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()) {
+
+                        Log.d("", "signInWithEmail:success");
+                        FirebaseUser user = mAuth.getCurrentUser();
+
+                        startActivity(new Intent(LogInActivity.this, MainActivity.class));
+                    } else {
+                        Log.w("", "signInWithEmail:failure", task.getException());
+                        Toast.makeText(LogInActivity.this, "Authentication failed." + task.getException().getMessage(),
+                                Toast.LENGTH_SHORT).show();
+
+                    }
+
+                }
+
+            });
+        }
+    }
+
 }
---
+++ app/src/main/java/com/example/assignment3/RegisterActivity.java
@@ -0,0 +1,105 @@
+package com.example.assignment3;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    EditText emailtxt;
+    EditText passwordtxt;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        emailtxt = findViewById(R.id.rEmail);
+        final String email = emailtxt.getText().toString();
+        mAuth = FirebaseAuth.getInstance();
+
+        TextView tvSignIn = findViewById(R.id.rLink);
+
+
+        tvSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                Intent i = new Intent(RegisterActivity.this, LogInActivity.class);
+                startActivity(i);
+
+
+            }
+        });
+
+    }
+
+    public void register(View view) {
+
+        emailtxt = findViewById(R.id.rEmail);
+        passwordtxt = findViewById(R.id.rPassword);
+
+
+        String email = emailtxt.getText().toString();
+        String pw = passwordtxt.getText().toString();
+        mAuth = FirebaseAuth.getInstance();
+
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
+        if (email.isEmpty()) {
+            emailtxt.setError("Please enter an email");
+            emailtxt.requestFocus();
+        } else if (pw.isEmpty()) {
+            passwordtxt.setError("Please enter password");
+            passwordtxt.requestFocus();
+        } else if (email.isEmpty() && pw.isEmpty()) {
+            Toast.makeText(RegisterActivity.this, "Please Enter an Email and Password to Register", Toast.LENGTH_LONG).show();
+        } else if (!((email.isEmpty() && pw.isEmpty()))) {
+            Log.d("", "createAccount:" + email);
+            mAuth = FirebaseAuth.getInstance();
+
+            // [START create_user_with_email]
+            mAuth.createUserWithEmailAndPassword(email, pw)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+
+                                Log.d("", "createUserWithEmail:success");
+                                FirebaseUser user = mAuth.getCurrentUser();
+
+                                Intent i = new Intent(RegisterActivity.this, MainActivity.class);
+                                startActivity(i);
+
+                            } else {
+                                Log.w("", "createUserWithEmail:failure", task.getException());
+
+                                Toast.makeText(RegisterActivity.this, "Authentication failed." + task.getException().getMessage(),
+                                        Toast.LENGTH_LONG).show();
+
+                            }
+
+
+                        }
+                    });
+
+        }
+    }
+
+
+}
